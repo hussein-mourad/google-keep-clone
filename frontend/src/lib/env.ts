91ecc2a27c2bcc809990a7b3ffb33e62b59a72e8
Hussein Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2026-07-13 00:33:04 +0300

    feat(frontend): inital setup

---
+++ frontend/src/lib/env.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+
+export const envSchema = z.object({
+  VITE_BACKEND_URL: z.url(),
+});
+
+export function validateEnv(config: Record<string, any>) {
+  try {
+    const result = envSchema.parse(config);
+    return result;
+  } catch (error) {
+    const issues = (error as z.ZodError).issues.map((issue) => {
+      return `Field: ${issue.path.join(".")} - ${issue.message}`;
+    });
+    throw new Error(`Env validation error: \n${issues.join("\n")}`);
+  }
+}
+
+const env = validateEnv(import.meta.env);
+
+export type Env = z.infer<typeof envSchema>;
+export type EnvKeys = keyof Env;
+export default env;
