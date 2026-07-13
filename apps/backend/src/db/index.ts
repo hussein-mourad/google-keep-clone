4da9c01c0776b788ee6531cc1727acca530623f1
Hussein Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2026-07-13 19:37:45 +0300

    refactor: use env global variable

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
+import env from "@/lib/env";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+export const db = drizzle(env.DATABASE_URL, { schema });
--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -11,12 +11,12 @@ export const auth = betterAuth({
   emailAndPassword: { enabled: true },
   socialProviders: {
     github: {
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      clientId: env.GITHUB_CLIENT_ID!,
+      clientSecret: env.GITHUB_CLIENT_SECRET!,
     },
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: env.GOOGLE_CLIENT_ID!,
+      clientSecret: env.GOOGLE_CLIENT_SECRET!,
     },
   },
 });
